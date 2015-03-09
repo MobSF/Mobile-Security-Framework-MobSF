1d640dcb480bcae56ee813f5e69b9223878d08d3
Ajin Abraham <ajin25@gmail.com>
2015-03-10 00:24:49 +0530

    Update settings.py

--- YodleeMobSec/settings.py
+++ YodleeMobSec/settings.py
@@ -48,12 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
-'''
-Not the right way of doing it. Quick Fix
-'django.middleware.security.SecurityMiddleware',
-)
-'''
+
 ROOT_URLCONF = 'YodleeMobSec.urls'
 
 WSGI_APPLICATION = 'YodleeMobSec.wsgi.application'
