ef3b476fd8ef9e90bb69202b11f1fc60493181c3
Dominik Schlecht <dominik.schlecht@hotmail.de>
2016-08-03 22:02:49 +0200

    Working state: XML-Parsing and representation

--- StaticAnalyzer/views/windows.py
+++ StaticAnalyzer/views/windows.py
@@ -76,7 +76,8 @@ def staticanalyzer_windows(request):
                     print "[INFO] Extracting APPX"
                     Unzip(APP_PATH, APP_DIR)
                     # BIN_NAME, BIN_ANAL, STRINGS = BinaryAnalysis(BIN_DIR, TOOLS_DIR, APP_DIR)
-                    BIN_NAME, VER = BinaryAnalysis(TOOLS_DIR, APP_DIR)
+                    xml_dic = parse_xml(APP_DIR)
+                    BIN_NAME = binary_analysis(TOOLS_DIR, APP_DIR)
                     # Saving to DB
                     print "\n[INFO] Connecting to DB"
                     if rescan == '1':
@@ -86,12 +87,22 @@ def staticanalyzer_windows(request):
                         ).update(
                             TITLE='Static Analysis',
                             APP_NAME=APP_NAME,
+                            PUB_NAME=xml_dic['pub_name'],
                             SIZE=SIZE,
                             MD5=MD5,
                             SHA1=SHA1,
                             SHA256=SHA256,
                             BINNAME=BIN_NAME,
-                            VERSION=VER,
+                            VERSION= xml_dic['version'],
+                            ARCH=xml_dic['arch'],
+                            COMPILER_VERSION=xml_dic['compiler_version'],
+                            VISUAL_STUDIO_VERSION=xml_dic['visual_studio_version'],
+                            VISUAL_STUDIO_EDITION=xml_dic['visual_studio_edition'],
+                            TARGET_OS=xml_dic['target_os'],
+                            APPX_DLL_VERSION=xml_dic['appx_dll_version'],
+                            PROJ_GUID=xml_dic['proj_guid'],
+                            OPTI_TOOL=xml_dic['opti_tool'],
+                            TARGET_RUN=xml_dic['target_run'],
                             # BIN_ANAL=BIN_ANAL,
                             # STRINGS=STRINGS
                         )
@@ -100,12 +111,22 @@ def staticanalyzer_windows(request):
                         STATIC_DB = StaticAnalyzerWindows(
                             TITLE='Static Analysis',
                             APP_NAME=APP_NAME,
+                            PUB_NAME=xml_dic['pub_name'],
                             SIZE=SIZE,
                             MD5=MD5,
                             SHA1=SHA1,
                             SHA256=SHA256,
                             BINNAME=BIN_NAME,
-                            VERSION=VER,
+                            VERSION= xml_dic['version'],
+                            ARCH=xml_dic['arch'],
+                            COMPILER_VERSION=xml_dic['compiler_version'],
+                            VISUAL_STUDIO_VERSION=xml_dic['visual_studio_version'],
+                            VISUAL_STUDIO_EDITION=xml_dic['visual_studio_edition'],
+                            TARGET_OS=xml_dic['target_os'],
+                            APPX_DLL_VERSION=xml_dic['appx_dll_version'],
+                            PROJ_GUID=xml_dic['proj_guid'],
+                            OPTI_TOOL=xml_dic['opti_tool'],
+                            TARGET_RUN=xml_dic['target_run'],
                             #BIN_ANAL=BIN_ANAL,
                             #STRINGS=STRINGS
                         )
@@ -113,12 +134,22 @@ def staticanalyzer_windows(request):
                     context = {
                         'title' : 'Static Analysis',
                         'name' : APP_NAME,
+                        'pub_name' : xml_dic['pub_name'],
                         'size' : SIZE,
                         'md5': MD5,
                         'sha1' : SHA1,
                         'sha256' : SHA256,
                         'bin_name' : BIN_NAME,
-                        'version' : VER,
+                        'version' : xml_dic['version'],
+                        'arch' : xml_dic['arch'],
+                        'compiler_version' : xml_dic['compiler_version'],
+                        'visual_studio_version' : xml_dic['visual_studio_version'],
+                        'visual_studio_edition' : xml_dic['visual_studio_edition'],
+                        'target_os' : xml_dic['target_os'],
+                        'appx_dll_version' : xml_dic['appx_dll_version'],
+                        'proj_guid' : xml_dic['proj_guid'],
+                        'opti_tool' : xml_dic['opti_tool'],
+                        'target_run' : xml_dic['target_run'],
                         #'bin_anal' : BIN_ANAL,
                         #'strings' : STRINGS,
                     }
@@ -138,169 +169,76 @@ def staticanalyzer_windows(request):
         template = "error.html"
         return render(request, template, context)
 
-def BinaryAnalysis(TOOLS_DIR, APP_DIR):
-    try:
-        print "[INFO] Starting Binary Analysis"
-        dirs = os.listdir(APP_DIR)
-        for d in dirs:
-            if d.endswith(".exe"):
-                break
-        BIN_DIR = os.path.join(APP_DIR, d)         #Full Dir/Payload/x.app
-        xml_file = os.path.join(APP_DIR, "AppxManifest.xml")
-        BIN = d.replace(".exe", "")
-        bin_name = BIN
+def binary_analysis(TOOLS_DIR, APP_DIR):
+    """Start binary analsis."""
+    print "[INFO] Starting Binary Analysis - XML"
+    dirs = os.listdir(APP_DIR)
+    for d in dirs:
+        if d.endswith(".exe"):
+            break
+    BIN_DIR = os.path.join(APP_DIR, d)         #Full Dir/Payload/x.app
 
-        ID = ""
-        ver = ""
-        SDK = ""
-        PLTFM = ""
-        MIN = ""
-        XML = ""
+    xml_file = os.path.join(APP_DIR, "AppxManifest.xml")
+    xml_dic = {}
+
+    BIN = d.replace(".exe", "")
+    bin_name = BIN
+    return bin_name
+
+def parse_xml(APP_DIR):
+    """Parse the AppxManifest file to get basic informations."""
+    try:
+        print "[INFO] Starting Binary Analysis - XML"
+        xml_file = os.path.join(APP_DIR, "AppxManifest.xml")
+        xml_dic = {}
 
         try:
             print "[INFO] Reading AppxManifest"
-            # TODO (Parse XML)
+            # TODO (Parse XML, display)
             config = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
             xml = etree.XML(open(xml_file).read(), config)
             for child in xml.getchildren():
-                if child.tag.endswith("}Identity"):
+                if child.tag.endswith("}Identity"): # } to prevent conflict with PhoneIdentity..
                     print "[*] Found Identity"
-                    ver = child.get("Version")
-                    print ver
-
+                    xml_dic['version'] = child.get("Version")
+                    xml_dic['arch'] = child.get("ProcessorArchitecture")
+                elif child.tag.endswith("Properties"):
+                    print("Found Properties")
+                    for sub_child in child.getchildren():
+                        if sub_child.tag.endswith("}DisplayName"):
+                            xml_dic['app_name'] = sub_child.text # TODO(Needed? Compare to existing app_name)
+                        elif sub_child.tag.endswith("}PublisherDisplayName"):
+                            xml_dic['pub_name'] = sub_child.text
+                elif child.tag.endswith("}Metadata"):
+                    print "[*] Metadata found"
+                    for sub_child in child.getchildren():
+                        if sub_child.get('Name') == "cl.exe":
+                            print "[*] compiler_version found"
+                            xml_dic['compiler_version'] = sub_child.get('Version')
+                        elif sub_child.get('Name') == "VisualStudio":
+                            print "[*] visual_studio_version found"
+                            xml_dic['visual_studio_version'] = sub_child.get('Version')
+                        elif sub_child.get('Name') == "VisualStudioEdition":
+                            print "[*] visual_studio_edition found"
+                            xml_dic['visual_studio_edition'] = sub_child.get('Value')
+                        elif sub_child.get('Name') == "OperatingSystem":
+                            print "[*] target_os found"
+                            xml_dic['target_os'] = sub_child.get('Version')
+                        elif sub_child.get('Name') == "Microsoft.Build.AppxPackage.dll":
+                            print "[*] appx_dll_version found"
+                            xml_dic['appx_dll_version'] = sub_child.get('Version')
+                        elif sub_child.get('Name') == "ProjectGUID":
+                            print "[*] proj_guid found"
+                            xml_dic['proj_guid'] = sub_child.get('Value')
+                        elif sub_child.get('Name') == "OptimizingToolset":
+                            print "[*] opti_tool found"
+                            xml_dic['opti_tool'] = sub_child.get('Value')
+                        elif sub_child.get('Name') == "TargetRuntime":
+                            print "[*] target_run found"
+                            xml_dic['target_run'] = sub_child.get('Value')
             # p = plistlib.readPlistFromString(XML)
-
         except:
             PrintException("[ERROR] - Reading from AppxManifest.xml")
-        return bin_name, ver
-        BIN_PATH = os.path.join(BIN_DIR, BIN)  #Full Dir/Payload/x.app/x
-        print "[INFO] iOS Binary : " + BIN
-        print "[INFO] Running otool against the Binary"
-        #Libs Used
-        LIBS = ''
-        if len(settings.OTOOL_BINARY) > 0 and isFileExists(OTOOL_BINARY):
-            OTOOL = settings.OTOOL_BINARY
-        else:
-            OTOOL = "otool"
-        args = [OTOOL, '-L', BIN_PATH]
-        dat = subprocess.check_output(args)
-        dat = escape(dat.replace(BIN_DIR + "/", ""))
-        LIBS = dat.replace("\n", "</br>")
-        #PIE
-        args = [OTOOL, '-hv', BIN_PATH]
-        dat = subprocess.check_output(args)
-        if "PIE" in dat:
-            PIE = "<tr><td><strong>fPIE -pie</strong> flag is Found</td><td><span class='label label-success'>Secure</span></td><td>App is compiled with Position Independent Executable (PIE) flag. This enables Address Space Layout Randomization (ASLR), a memory protection mechanism for exploit mitigation.</td></tr>"
-        else:
-            PIE = "<tr><td><strong>fPIE -pie</strong> flag is not Found</td><td><span class='label label-danger'>Insecure</span></td><td>App is not compiled with Position Independent Executable (PIE) flag. So Address Space Layout Randomization (ASLR) is missing. ASLR is a memory protection mechanism for exploit mitigation.</td></tr>"
-        #Stack Smashing Protection & ARC
-        args = [OTOOL, '-Iv', BIN_PATH]
-        dat = subprocess.check_output(args)
-        if "stack_chk_guard" in dat:
-            SSMASH = "<tr><td><strong>fstack-protector-all</strong> flag is Found</td><td><span class='label label-success'>Secure</span></td><td>App is compiled with Stack Smashing Protector (SSP) flag and is having protection against Stack Overflows/Stack Smashing Attacks.</td></tr>"
-        else:
-            SSMASH = "<tr><td><strong>fstack-protector-all</strong> flag is not Found</td><td><span class='label label-danger'>Insecure</span></td><td>App is not compiled with Stack Smashing Protector (SSP) flag. It is vulnerable to Stack Overflows/Stack Smashing Attacks.</td></tr>"
-        #ARC
-        if "_objc_release" in dat:
-            ARC = "<tr><td><strong>fobjc-arc</strong> flag is Found</td><td><span class='label label-success'>Secure</span></td><td>App is compiled with Automatic Reference Counting (ARC) flag. ARC is a compiler feature that provides automatic memory management of Objective-C objects and is an exploit mitigation mechanism against memory corruption vulnerabilities.</td></tr>"
-        else:
-            ARC = "<tr><td><strong>fobjc-arc</strong> flag is not Found</td><td><span class='label label-danger'>Insecure</span></td><td>App is not compiled with Automatic Reference Counting (ARC) flag. ARC is a compiler feature that provides automatic memory management of Objective-C objects and protects from memory corruption vulnerabilities.</td></tr>"
-        ##########
-        BANNED_API = ''
-        x = re.findall("alloca|gets|memcpy|printf|scanf|sprintf|sscanf|strcat|StrCat|strcpy|StrCpy|strlen|StrLen|strncat|StrNCat|strncpy|StrNCpy|strtok|swprintf|vsnprintf|vsprintf|vswprintf|wcscat|wcscpy|wcslen|wcsncat|wcsncpy|wcstok|wmemcpy", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            BANNED_API = "<tr><td>Binary make use of banned API(s)</td><td><span class='label label-danger'>Insecure</span></td><td>The binary may contain the following banned API(s) </br><strong>" + str(x) + "</strong>.</td></tr>"
-        WEAK_CRYPTO = ''
-        x = re.findall("kCCAlgorithmDES|kCCAlgorithm3DES||kCCAlgorithmRC2|kCCAlgorithmRC4|kCCOptionECBMode|kCCOptionCBCMode", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            WEAK_CRYPTO = "<tr><td>Binary make use of some Weak Crypto API(s)</td><td><span class='label label-danger'>Insecure</span></td><td>The binary may use the following weak crypto API(s)</br><strong>" + str(x) + "</strong>.</td></tr>"
-        CRYPTO = ''
-        x = re.findall("CCKeyDerivationPBKDF|CCCryptorCreate|CCCryptorCreateFromData|CCCryptorRelease|CCCryptorUpdate|CCCryptorFinal|CCCryptorGetOutputLength|CCCryptorReset|CCCryptorRef|kCCEncrypt|kCCDecrypt|kCCAlgorithmAES128|kCCKeySizeAES128|kCCKeySizeAES192|kCCKeySizeAES256|kCCAlgorithmCAST|SecCertificateGetTypeID|SecIdentityGetTypeID|SecKeyGetTypeID|SecPolicyGetTypeID|SecTrustGetTypeID|SecCertificateCreateWithData|SecCertificateCreateFromData|SecCertificateCopyData|SecCertificateAddToKeychain|SecCertificateGetData|SecCertificateCopySubjectSummary|SecIdentityCopyCertificate|SecIdentityCopyPrivateKey|SecPKCS12Import|SecKeyGeneratePair|SecKeyEncrypt|SecKeyDecrypt|SecKeyRawSign|SecKeyRawVerify|SecKeyGetBlockSize|SecPolicyCopyProperties|SecPolicyCreateBasicX509|SecPolicyCreateSSL|SecTrustCopyCustomAnchorCertificates|SecTrustCopyExceptions|SecTrustCopyProperties|SecTrustCopyPolicies|SecTrustCopyPublicKey|SecTrustCreateWithCertificates|SecTrustEvaluate|SecTrustEvaluateAsync|SecTrustGetCertificateCount|SecTrustGetCertificateAtIndex|SecTrustGetTrustResult|SecTrustGetVerifyTime|SecTrustSetAnchorCertificates|SecTrustSetAnchorCertificatesOnly|SecTrustSetExceptions|SecTrustSetPolicies|SecTrustSetVerifyDate|SecCertificateRef|SecIdentityRef|SecKeyRef|SecPolicyRef|SecTrustRef", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            CRYPTO = "<tr><td>Binary make use of the following Crypto API(s)</td><td><span class='label label-info'>Info</span></td><td>The binary may use the following crypto API(s)</br><strong>" + str(x) + "</strong>.</td></tr>"
-        WEAK_HASH = ''
-        x = re.findall("CC_MD2_Init|CC_MD2_Update|CC_MD2_Final|CC_MD2|MD2_Init|MD2_Update|MD2_Final|CC_MD4_Init|CC_MD4_Update|CC_MD4_Final|CC_MD4|MD4_Init|MD4_Update|MD4_Final|CC_MD5_Init|CC_MD5_Update|CC_MD5_Final|CC_MD5|MD5_Init|MD5_Update|MD5_Final|MD5Init|MD5Update|MD5Final|CC_SHA1_Init|CC_SHA1_Update|CC_SHA1_Final|CC_SHA1|SHA1_Init|SHA1_Update|SHA1_Final", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            WEAK_HASH = "<tr><td>Binary make use of the following Weak HASH API(s)</td><td><span class='label label-danger'>Insecure</span></td><td>The binary may use the following weak hash API(s)</br><strong>" + str(x) + "</strong>.</td></tr>"
-        HASH = ''
-        x = re.findall("CC_SHA224_Init|CC_SHA224_Update|CC_SHA224_Final|CC_SHA224|SHA224_Init|SHA224_Update|SHA224_Final|CC_SHA256_Init|CC_SHA256_Update|CC_SHA256_Final|CC_SHA256|SHA256_Init|SHA256_Update|SHA256_Final|CC_SHA384_Init|CC_SHA384_Update|CC_SHA384_Final|CC_SHA384|SHA384_Init|SHA384_Update|SHA384_Final|CC_SHA512_Init|CC_SHA512_Update|CC_SHA512_Final|CC_SHA512|SHA512_Init|SHA512_Update|SHA512_Final", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            HASH = "<tr><td>Binary make use of the following HASH API(s)</td><td><span class='label label-info'>Info</span></td><td>The binary may use the following hash API(s)</br><strong>" + str(x) + "</strong>.</td></tr>"
-        RAND = ''
-        x = re.findall("srand|random", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            RAND = "<tr><td>Binary make use of the insecure Random Function(s)</td><td><span class='label label-danger'>Insecure</span></td><td>The binary may use the following insecure Random Function(s)</br><strong>" + str(x) + "</strong>.</td></tr>"
-        LOG = ''
-        x = re.findall("NSLog", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            LOG = "<tr><td>Binary make use of Logging Function</td><td><span class='label label-info'>Info</span></td><td>The binary may use <strong>NSLog</strong> function for logging.</td></tr>"
-        MALL = ''
-        x = re.findall("malloc", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            MALL = "<tr><td>Binary make use of <strong>malloc</strong> Function</td><td><span class='label label-danger'>Insecure</span></td><td>The binary may use <strong>malloc</strong> function instead of <strong>calloc</strong>.</td></tr>"
-        DBG = ''
-        x = re.findall("ptrace", dat)
-        x = list(set(x))
-        x = ', '.join(x)
-        if len(x) > 1:
-            DBG = "<tr><td>Binary calls <strong>ptrace</strong> Function for anti-debugging.</td><td><span class='label label-warning'>warning</span></td><td>The binary may use <strong>ptrace</strong> function. It can be used to detect and prevent debuggers. Ptrace is not a public API and Apps that use non-public APIs will be rejected from AppStore. </td></tr>"
-        CDUMP = ''
-        WVIEW = ''
-        try:
-            print "[INFO] Running class-dump-z against the Binary"
-            if len(settings.CLASSDUMPZ_BINARY) > 0 and isFileExists(settings.CLASSDUMPZ_BINARY):
-                CLASSDUMPZ_BIN = settings.CLASSDUMPZ_BINARY
-            else:
-                CLASSDUMPZ_BIN = os.path.join(TOOLS_DIR, 'class-dump-z')
-            subprocess.call(["chmod", "777", CLASSDUMPZ_BIN])
-            dat = subprocess.check_output([CLASSDUMPZ_BIN, BIN_PATH])
-            CDUMP = dat
-            FILE = os.path.join(APP_DIR, "classdump.txt")
-            with open(FILE, "w") as f:
-                f.write(CDUMP)
-            if "UIWebView" in CDUMP:
-                WVIEW = "<tr><td>Binary uses WebView Component.</td><td><span class='label label-info'>Info</span></td><td>The binary may use WebView Component.</td></tr>"
-
-        except Exception:
-            PrintException("[ERROR] - Cannot perform class dump")
-        BIN_RES = \
-            PIE + \
-            SSMASH + \
-            ARC + \
-            BANNED_API + \
-            WEAK_CRYPTO + \
-            CRYPTO + \
-            WEAK_HASH + \
-            HASH + \
-            RAND + \
-            LOG + \
-            MALL + \
-            DBG + \
-            WVIEW
-        #classdump
-
-        # strings
-        args = ["strings", BIN_PATH]
-        strings = subprocess.check_output(args)
-        strings = escape(strings.replace(BIN_DIR + "/", ""))
-        STRINGS = strings.replace("\n", "</br>")
-
-        return XML, BIN_NAME, ID, VER, SDK, PLTFM, MIN, LIBS, BIN_RES, STRINGS
+        return xml_dic
     except Exception:
         PrintException("[ERROR] iOS Binary Analysis")
