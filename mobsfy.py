882bd870442264b952fd4321c54fb84fb2d1e9c2
Cristhian Parrot <cparrot@hackerzvoice.net>
2016-09-30 00:42:48 +0200

    Resolving issue when there are multiple devices connected

--- mobsfy.py
+++ mobsfy.py
@@ -50,7 +50,7 @@ try:
     ExecuteCMD([adb, "kill-server"])
     ExecuteCMD([adb, "start-server"])
     ExecuteCMD([adb, "connect",adbconnect])
-    ExecuteCMD([adb, "wait-for-device"])
+    ExecuteCMD([adb, "-s",adbconnect, "wait-for-device"])
     #Install MITM RootCA
     ExecuteCMD([adb, "-s",adbconnect ,"push", ROOTCA, "/data/local/tmp/0025aabb.0"])
     ExecuteCMD([adb, "-s",adbconnect ,"shell", "su", "-c", "mount", "-o", "rw,remount,rw", "/system"])
