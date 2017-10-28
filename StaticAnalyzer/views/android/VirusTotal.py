67a29405e6f10bf3e2bda712cddd6e83a4d38b0b
superpoussin22 <vincent.nadal@orange.fr>
2017-10-28 18:41:21 +0200

    Update VirusTotal.py
    take remark in to account

--- StaticAnalyzer/views/android/VirusTotal.py
+++ StaticAnalyzer/views/android/VirusTotal.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class VirusTotal:
 
     base_url = 'https://www.virustotal.com/vtapi/v2/file/'
-
+    
     def get_report(self, file_hash):
         '''
         :param file_hash: md5/sha1/sha256
@@ -28,44 +28,43 @@ class VirusTotal:
             except:
                 print "[ERROR] VirusTotal ConnectionError, check internet connectivity"
                 return None
-
+    
             json_response = response.json()
             return json_response
-
+    
         except:
             PrintException("[ERROR] in VirusTotal get_report")
             return None
-
+    
     def upload_file(self, file_path):
         '''
         :param file_path: file path to upload
         :return: json response / None
         '''
-        if settings.VT_UPLOAD:
+        try:
+            url = self.base_url + "scan"
+            files = {
+                'file': open(file_path, 'rb')
+            }
+            headers = {
+                "apikey": settings.VT_API_KEY
+            }
             try:
-                url = self.base_url + "scan"
-                files = {
-                    'file': open(file_path, 'rb')
-                }
-                headers = {
-                    "apikey": settings.VT_API_KEY
-                }
-                try:
-                    response = requests.post(url, files=files, data=headers)
-                    if response.status_code == 403:
-                        print "[ERROR] VirusTotal Permission denied, wrong api key?"
-                        return None
-                except:
-                    print "[ERROR] VirusTotal ConnectionError, check internet connectivity"
+                response = requests.post(url, files=files, data=headers)
+                if response.status_code == 403:
+                    print "[ERROR] VirusTotal Permission denied, wrong api key?"
                     return None
-                json_response = response.json()
-                return json_response
-
             except:
-               PrintException("[ERROR] in VirusTotal upload_file")
-               return None
-        return None
-
+                print "[ERROR] VirusTotal ConnectionError, check internet connectivity"
+                return None
+            json_response = response.json()
+            return json_response
+    
+        except:
+            PrintException("[ERROR] in VirusTotal upload_file")
+            return None
+    
+    
     def get_result(self, file_path, file_hash):
         '''
         Uoloading a file and getting the approval msg from VT or fetching existing report
@@ -82,10 +81,13 @@ class VirusTotal:
                     print "[INFO] VirusTotal: " + report['verbose_msg']
                     return report
             print "[INFO] VirusTotal: " + report['verbose_msg']
-            print "[INFO] VirusTotal: file upload"
-            upload_response = self.upload_file(file_path)
-            if upload_response:
-                print "[INFO] VirusTotal: " + upload_response['verbose_msg']
-            return upload_response
+            if settings.VT_UPLOAD:
+                print "[INFO] VirusTotal: file upload"
+                upload_response = self.upload_file(file_path)
+                if upload_response:
+                    print "[INFO] VirusTotal: " + upload_response['verbose_msg']
+                return upload_response
+            else:
+                return None     
         except:
             PrintException("[ERROR] in VirusTotal get_result")
