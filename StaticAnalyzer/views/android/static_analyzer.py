730c9ae6f64bdd0ddd4accd189bf5d4f83c48e52
Abraham McCarthy <ramimac@users.noreply.github.com>
2017-12-15 12:13:43 -0500

    Fixed #476 (#515)
    Add extra warning and fixes #476

--- StaticAnalyzer/views/android/static_analyzer.py
+++ StaticAnalyzer/views/android/static_analyzer.py
@@ -265,6 +265,7 @@ def static_analyzer(request, api=False):
                 cert_dic = {}
                 cert_dic['cert_info'] = ''
                 cert_dic['issued'] = ''
+                cert_dic['sha256Digest'] = False
                 bin_an_buff = []
                 app_dic['strings'] = ''
                 app_dic['zipped'] = ''
