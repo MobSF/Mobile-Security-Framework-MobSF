6389cf48c040bd1509bc729d7fdcdf79fc8beb0e
superpoussin22 <vincent.nadal@orange.fr>
2018-03-13 13:23:23 +0100

    Update manifest_analysis.py

--- StaticAnalyzer/views/android/manifest_analysis.py
+++ StaticAnalyzer/views/android/manifest_analysis.py
@@ -1257,7 +1257,7 @@ def read_manifest(app_dir, tools_dir, typ, binary):
     """Read the manifest file."""
     try:
         dat = ''
-
+        manifest = ''
         if binary is True:
             print("[INFO] Getting Manifest from Binary")
             manifest = os.path.join(app_dir, "apktool_out", "AndroidManifest.xml")
