e489bb6d3849d79019295b17c9084339352132a3
Ajin Abraham <ajin25@gmail.com>
2018-05-09 15:20:43 +0530

    updated JAVA check

--- MobSF/utils.py
+++ MobSF/utils.py
@@ -88,7 +88,7 @@ def printMobSFverison():
 
 def check_update():
     try:
-        print("\n[INFO] Checking for Update.")
+        print("[INFO] Checking for Update.")
         github_url = "https://raw.githubusercontent.com/MobSF/Mobile-Security-Framework-MobSF/master/MobSF/settings.py"
         try:
             proxies, verify = upstream_proxy('https')
@@ -237,26 +237,50 @@ def FindVbox(debug=False):
         if debug:
             PrintException("[ERROR] Cannot find VirtualBox path.")
 
-# Maintain JDK Version
-JAVA_VER = '1.7|1.8|1.9|2.0|2.1|2.2|2.3'
-
 
 def FindJava(debug=False):
+    """ Find Java """
+    # Maintain JDK Version
+    java_versions = '1.7|1.8|1.9|2.0|2.1|2.2|2.3|8|9'
+    """
+    This code is needed because some people are not capable
+    of setting java path :-(
+    """
+    win_java_paths = [
+        "C:/Program Files/Java/",
+        "C:/Program Files (x86)/Java/",
+        "D:/Program Files/Java/",
+        "D:/Program Files (x86)/Java/",
+        "E:/Program Files/Java/",
+        "E:/Program Files (x86)/Java/",
+        "F:/Program Files/Java/",
+        "F:/Program Files (x86)/Java/",
+        "G:/Program Files/Java/",
+        "G:/Program Files (x86)/Java/",
+        "H:/Program Files/Java/",
+        "H:/Program Files (x86)/Java/",
+        "I:/Program Files/Java/",
+        "I:/Program Files (x86)/Java/",
+    ]
     try:
         err_msg1 = "[ERROR] Oracle JDK 1.7 or above is not found!"
-        if len(settings.JAVA_DIRECTORY) > 0 and isDirExists(settings.JAVA_DIRECTORY):
-            return settings.JAVA_DIRECTORY
-        if platform.system() == "Windows":
+        if isDirExists(settings.JAVA_DIRECTORY):
+            if settings.JAVA_DIRECTORY.endswith("/"):
+                return settings.JAVA_DIRECTORY
+            elif settings.JAVA_DIRECTORY.endswith("\\"):
+                return settings.JAVA_DIRECTORY
+            else:
+                return settings.JAVA_DIRECTORY + "/"
+        elif platform.system() == "Windows":
             if debug:
                 print("\n[INFO] Finding JDK Location in Windows....")
             # JDK 7 jdk1.7.0_17/bin/
-            for java_path in ["C:/Program Files/Java/",
-                              "C:/Program Files (x86)/Java/"]:
+            for java_path in win_java_paths:
                 if os.path.isdir(java_path):
                     for dirname in os.listdir(java_path):
                         if "jdk" in dirname:
                             win_java_path = java_path + dirname + "/bin/"
-                            args = [win_java_path + "java", "--version"]
+                            args = [win_java_path + "java", "-version"]
                             dat = RunProcess(args)
                             if "java" in dat:
                                 if debug:
@@ -267,7 +291,7 @@ def FindJava(debug=False):
                 java_home = os.environ.get(env)
                 if java_home and os.path.isdir(java_home):
                     win_java_path = java_home + "/bin/"
-                    args = [win_java_path + "java", "--version"]
+                    args = [win_java_path + "java", "-version"]
                     dat = RunProcess(args)
                     if "java" in dat:
                         if debug:
@@ -279,31 +303,42 @@ def FindJava(debug=False):
             return "java"
         else:
             if debug:
-                print("\n[INFO] Finding JDK Location in Linux/MAC....")
-            MAC_LINUX_JAVA = "/usr/bin/"
-            args = [MAC_LINUX_JAVA + "java"]
+                print("[INFO] Finding JDK Location in Linux/MAC....")
+            # Check in Environment Variables
+            for env in ["JDK_HOME", "JAVA_HOME"]:
+                java_home = os.environ.get(env)
+                if java_home and os.path.isdir(java_home):
+                    lm_java_path = java_home + "/bin/"
+                    args = [lm_java_path + "java", "-version"]
+                    dat = RunProcess(args)
+                    if "oracle" in dat:
+                        if debug:
+                            print("\n[INFO] Oracle Java is installed!")
+                        return lm_java_path
+            mac_linux_java_dir = "/usr/bin/"
+            args = [mac_linux_java_dir + "java"]
             dat = RunProcess(args)
             if "oracle" in dat:
-                args = [MAC_LINUX_JAVA + "java", '-version']
+                args = [mac_linux_java_dir + "java", '-version']
                 dat = RunProcess(args)
                 f_line = dat.split("\n")[0]
-                if re.findall(JAVA_VER, f_line):
+                if re.findall(java_versions, f_line):
                     if debug:
-                        print("\n[INFO] JDK 1.7 or above is available")
-                    return MAC_LINUX_JAVA
+                        print("[INFO] JDK 1.7 or above is available")
+                    return mac_linux_java_dir
                 else:
                     err_msg = "[ERROR] Please install Oracle JDK 1.7 or above"
                     if debug:
                         print(Color.BOLD + Color.RED + err_msg + Color.END)
-                    return ""
+                        return "java"
             else:
                 if debug:
                     print(Color.BOLD + Color.RED + err_msg1 + Color.END)
-                return ""
+                    return "java"
     except:
         if debug:
             PrintException("[ERROR] Oracle Java (JDK >=1.7) is not found!")
-        return ""
+        return "java"
 
 
 def RunProcess(args):
