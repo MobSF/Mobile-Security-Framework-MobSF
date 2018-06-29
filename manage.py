da54c33903bd07973a2edd4077c7dfdf2339266f
Mohammadali toofani <toofanimohammadali@yahoo.com>
2018-06-29 17:45:38 +0430

    test.

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MobSF.settings")
     from django.core.management import execute_from_command_line
-
+#----- Test
     execute_from_command_line(sys.argv)
