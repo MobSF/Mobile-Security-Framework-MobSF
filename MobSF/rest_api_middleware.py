770d238548807fc745cae706e9bcc625e11a6db7
me <mamian521@gmail.com>
2018-08-21 18:39:25 +0800

    Add Auth Middleware (#690)
    * add rest_api_middleware
    
    * add DATA_UPLOAD_MAX_MEMORY_SIZE to 256MB
    
    * fixed open api_docs

---
+++ MobSF/rest_api_middleware.py
@@ -0,0 +1,14 @@
+
+from django.utils.deprecation import MiddlewareMixin
+from .rest_api import api_auth, make_api_response
+
+UNAUTHORIZED = 401
+
+class RestApiAuthMiddleware(MiddlewareMixin):
+    
+    def process_request(self, request):
+        if not request.path.startswith("/api/"):
+            return
+        if not api_auth(request.META):
+            return make_api_response(
+            {"error": "You are unauthorized to make this request."}, UNAUTHORIZED)
