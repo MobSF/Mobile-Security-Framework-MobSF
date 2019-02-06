e14489125029651b3b163eddc112cc1415d3f360
superpoussin22 <vincent.nadal@orange.fr>
2019-02-06 11:18:52 +0100

    clean message syntax
    remove [INFO] in message

--- StaticAnalyzer/views/android/VirusTotal.py
+++ StaticAnalyzer/views/android/VirusTotal.py
@@ -67,7 +67,7 @@ class VirusTotal:
                     logger.error("VirusTotal Permission denied, wrong api key?")
                     return None
             except:
-                logger.error("VirusTotal ConnectionError, check internet connectivity")
+                logger.error("VirusTotal Connection Error, check internet connectivity")
                 return None
             json_response = response.json()
             return json_response
@@ -84,18 +84,18 @@ class VirusTotal:
         :return: VirusTotal result json / None upon error
         """
         try:
-            logger.info("[INFO] VirusTotal: Check for existing report")
+            logger.info("VirusTotal: Check for existing report")
             report = self.get_report(file_hash)
             # Check for existing report
             if report:
                 if report['response_code'] == 1:
-                    logger.info("[INFO] VirusTotal: " + report['verbose_msg'])
+                    logger.info("VirusTotal: " + report['verbose_msg'])
                     return report
             if settings.VT_UPLOAD:
-                logger.info("[INFO] VirusTotal: file upload")
+                logger.info("VirusTotal: file upload")
                 upload_response = self.upload_file(file_path)
                 if upload_response:
-                    logger.info("[INFO] VirusTotal: {}".format(upload_response['verbose_msg']))
+                    logger.info("VirusTotal: {}".format(upload_response['verbose_msg']))
                 return upload_response
             else:
                 logger.info("MobSF: VirusTotal Scan not performed as file upload is disabled in settings.py. "
