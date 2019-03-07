3d98bb1de2cc02558e831607aa896cb7b3cae86f
superpoussin22 <vincent.nadal@orange.fr>
2019-03-07 13:46:38 +0100

    1st cleanup

--- scripts/update_apkid.sh
+++ scripts/update_apkid.sh
@@ -5,20 +5,15 @@ if [ "$script_path" != "scripts" ] && [ "$script_path" != "./scripts" ]; then
     exit  1
 fi
 current_dir=$(pwd)
-rules_dir=${current_dir}/venv/lib/python3.6/site-packages/apkid/rules/
+rules_dir=${current_dir}/root/Mobile-Security-Framework-MobSF/MalwareAnalyzer/
 virtualenv venv -p python3
 source venv/bin/activate
 platform='unknown'
 unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
-  export ARCHFLAGS="-arch x86_64"
-  export LDFLAGS="-L/usr/local/opt/openssl/lib"
-  export CFLAGS="-I/usr/local/opt/openssl/include"
-fi
 git clone https://github.com/rednaga/APKiD.git 
 cd APKiD 
 python3 prep-release.py 
-cp apkid/rules/rules.yarc ../venv/lib/python3.6/site-packages/apkid/rules/  
+cp apkid/rules/rules.yarc ${rules_dfir}  
 cd ..
 if [[ "$unamestr" == 'Darwin' ]]; then
   sed -i ' ' "s#RULES_DIR =.*#RULES_DIR =  \"$rules_dir\"#" ./venv/lib/python3.6/site-packages/apkid/rules.py 
