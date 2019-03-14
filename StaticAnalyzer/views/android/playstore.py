1d797bfd2ca9cc21c2671ce6e6015e3a7c30b00e
Ajin Abraham <ajin25@gmail.com>
2019-03-13 20:54:05 -0400

    Fixed #895

--- StaticAnalyzer/views/android/playstore.py
+++ StaticAnalyzer/views/android/playstore.py
@@ -4,13 +4,14 @@ import play_scraper
 import logging
 logger = logging.getLogger(__name__)
 
+
 def get_app_details(package_id):
-	'''Get App Details form PlayStore'''
-	try:
-		logger.info("Fetching Details from Play Store: %s", package_id)
-		det = play_scraper.details(package_id)
-		det["error"] = False
-	except ValueError as vxp:
-		logger.warning("%s", vxp)
-		det = {"error": True}
-	return det
+    '''Get App Details form PlayStore'''
+    try:
+        logger.info("Fetching Details from Play Store: %s", package_id)
+        det = play_scraper.details(package_id)
+        det["error"] = False
+    except Exception as exp:
+        logger.warning("Unable to get app details. %s", exp)
+        det = {"error": True}
+    return det
