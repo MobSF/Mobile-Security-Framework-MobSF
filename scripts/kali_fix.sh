cbda87a709fc41ef89ce0367d9bfd6d4cd370e46
superpoussin22 <vincent.nadal@orange.fr>
2019-07-20 20:11:50 +0200

    little qa

--- scripts/kali_fix.sh
+++ scripts/kali_fix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Running Kali Fix to enable 32bit execution support"
+echo 'Running Kali Fix to enable 32bit execution support'
 export DEBIAN_FRONTEND=noninteractive
 dpkg --add-architecture i386
 apt-get update -q
-apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" libstdc++6:i386 zlib1g:i386 libncurses5:i386
+apt-get install -q -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' libstdc++6:i386 zlib1g:i386 libncurses5:i386
