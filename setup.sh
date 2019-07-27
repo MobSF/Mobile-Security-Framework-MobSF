8988c11d4a8eea99fd80fbe083f6bf2cd2daa234
superpoussin22 <vincent.nadal@orange.fr>
2019-07-27 19:13:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ if [[ "$unamestr" == 'Darwin' ]]; then
   is_installed=$(pkgutil --pkgs=com.apple.pkg.macOS_SDK_headers_for_macOS_${current_macos_version})
   if [ -z "$is_installed" ]; then 
       if [ "$major" -ge "10" ] && [ "$minor" -ge "14" ]; then 
-          echo 'Please install macOS headers to prevent apkid error 34'
+          echo 'Please install macOS headers.'
           echo 'sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_${current_macos_version}.pkg -target /'
           exit 1
       fi    
