934a0330066a56b1a4bfea2c028f3e54bd4f89b7
superpoussin22 <vincent.nadal@orange.fr>
2019-08-08 23:30:05 +0200

    Add Google Playstore Description

--- StaticAnalyzer/views/android/playstore.py
+++ StaticAnalyzer/views/android/playstore.py
@@ -13,6 +13,9 @@ def get_app_details(package_id):
         det = app(package_id)
         det.pop('descriptionHTML', None)
         det.pop('comments', None)
+        description = det['description'].replace('<b>', '')
+        description = description.replace('</b>', '\n')
+        det['description'] = description
         det['error'] = False
     except Exception:
         logger.warning('Unable to get app details.')
