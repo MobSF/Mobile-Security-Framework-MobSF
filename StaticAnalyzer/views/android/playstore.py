89f9fc5ebd35bc7cc34d57a5338ab5ee5a98c5ad
superpoussin22 <vincent.nadal@orange.fr>
2019-08-09 00:11:33 +0200

    Better Playstore Description

--- StaticAnalyzer/views/android/playstore.py
+++ StaticAnalyzer/views/android/playstore.py
@@ -1,5 +1,6 @@
 # -*- coding: utf_8 -*-
 from google_play_scraper import app
+from bs4 import BeautifulSoup
 
 import logging
 
@@ -13,9 +14,8 @@ def get_app_details(package_id):
         det = app(package_id)
         det.pop('descriptionHTML', None)
         det.pop('comments', None)
-        description = det['description'].replace('<b>', '')
-        description = description.replace('</b>', '\n')
-        det['description'] = description
+        description = BeautifulSoup(det['description'], features="lxml") 
+        det['description'] = description.get_text()
         det['error'] = False
     except Exception:
         logger.warning('Unable to get app details.')
