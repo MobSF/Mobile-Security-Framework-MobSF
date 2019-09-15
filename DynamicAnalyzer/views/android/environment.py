3f73c4bddae3967d807e66bc58ad3ea7070a40fe
superpoussin22 <vincent.nadal@orange.fr>
2019-09-15 11:30:20 +0200

    update frida and requirements

--- DynamicAnalyzer/views/android/environment.py
+++ DynamicAnalyzer/views/android/environment.py
@@ -375,7 +375,7 @@ class Environment:
         frida_dir = 'onDevice/frida/'
         frida_bin = os.path.join(self.tools_dir,
                                  frida_dir,
-                                 'frida-server-12.6.18-android-x86')
+                                 'frida-server-12.7.0-android-x86')
         arch = self.get_android_arch()
         logger.info('Android instance architecture identified as %s', arch)
         if 'x86' not in arch:
