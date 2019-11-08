8d862b67179cb49231fe78681107aeb4b2060303
Ajin Abraham <ajin25@gmail.com>
2019-11-08 11:22:02 -0500

    HOTFIX: temporary warning suppression for Python 3.8

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Django Manage."""
+import warnings
 import os
 import sys
+
+warnings.filterwarnings('ignore', category=UserWarning, module='cffi')
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MobSF.settings')
