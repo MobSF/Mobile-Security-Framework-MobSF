ceb4c04cef9bf69d5f8ddae2bb853c7da3f20c3e
Ajin Abraham <ajin25@gmail.com>
2019-11-10 14:23:00 -0500

    enable apkid

--- MobSF/settings.py
+++ MobSF/settings.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-MOBSF_VER = 'v2.0.5 Beta'
+MOBSF_VER = 'v2.0.6 Beta'
 
 BANNER = """
   __  __       _    ____  _____         ____    ___  
@@ -327,7 +327,7 @@ else:
     # MALWARE ANALYZER SETTINGS
     # --------------------------
     DOMAIN_MALWARE_SCAN = True
-    APKID_ENABLED = False
+    APKID_ENABLED = True
     # ==============================================
 
     # -----External URLS--------------------------
