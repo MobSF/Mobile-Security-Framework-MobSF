b50bacc98654ff2f12ab6f3b22e10df6df4e0c89
Karol Piątek <karol.piatek@netguru.com>
2020-02-28 08:51:35 +0100

    add swift case in code analysis

--- StaticAnalyzer/views/ios/code_analysis.py
+++ StaticAnalyzer/views/ios/code_analysis.py
@@ -5,7 +5,7 @@ import shutil
 
 from MalwareAnalyzer.views.domain_check import malware_check
 
-from StaticAnalyzer.views.ios import ios_apis, ios_rules
+from StaticAnalyzer.views.ios import ios_apis, objc_rules, swift_rules
 from StaticAnalyzer.views.shared_func import (api_rule_matcher,
                                               code_rule_matcher,
                                               url_n_email_extract)
@@ -14,12 +14,10 @@ logger = logging.getLogger(__name__)
 
 
 def ios_source_analysis(src):
-    """IOS Objective-C Code Analysis."""
+    """IOS Objective-C and Swift Code Analysis."""
     try:
         logger.info('Starting iOS Source Code and PLIST Analysis')
-        source_type = 'Swift'
-        api_rules = ios_apis.CODE_APIS
-        code_rules = ios_rules.CODE_RULES
+
         code_findings = {}
         api_findings = {}
         email_n_file = []
@@ -29,35 +27,43 @@ def ios_source_analysis(src):
 
         for dirname, _, files in os.walk(src):
             for jfile in files:
+
                 if jfile.endswith('.m'):
-                    source_type = 'Objective C'
-                    jfile_path = os.path.join(src, dirname, jfile)
-                    if '+' in jfile:
-                        new_path = os.path.join(
-                            src, dirname, jfile.replace('+', 'x'))
-                        shutil.move(jfile_path, new_path)
-                        jfile_path = new_path
-                    dat = ''
-                    with io.open(jfile_path,
-                                 mode='r',
-                                 encoding='utf8',
-                                 errors='ignore') as flip:
-                        dat = flip.read()
+                    api_rules = ios_apis.CODE_APIS
+                    code_rules = objc_rules.OBJC_RULES
+                elif jfile.endswith('.swift'):
+                    api_rules = ios_apis.CODE_APIS
+                    code_rules = swift_rules.SWIFT_RULES
+                else:
+                    continue
 
-                    # Code Analysis
-                    relative_src_path = jfile_path.replace(src, '')
-                    code_rule_matcher(code_findings, [], dat,
-                                      relative_src_path, code_rules)
-                    # API Analysis
-                    api_rule_matcher(api_findings, [], dat,
-                                     relative_src_path, api_rules)
+                jfile_path = os.path.join(src, dirname, jfile)
+                if '+' in jfile:
+                    new_path = os.path.join(
+                        src, dirname, jfile.replace('+', 'x'))
+                    shutil.move(jfile_path, new_path)
+                    jfile_path = new_path
+                dat = ''
+                with io.open(jfile_path,
+                             mode='r',
+                             encoding='utf8',
+                             errors='ignore') as flip:
+                    dat = flip.read()
 
-                    # Extract URLs and Emails
-                    urls, urls_nf, emails_nf = url_n_email_extract(
-                        dat, relative_src_path)
-                    url_list.extend(urls)
-                    url_n_file.extend(urls_nf)
-                    email_n_file.extend(emails_nf)
+                # Code Analysis
+                relative_src_path = jfile_path.replace(src, '')
+                code_rule_matcher(code_findings, [], dat,
+                                  relative_src_path, code_rules)
+                # API Analysis
+                api_rule_matcher(api_findings, [], dat,
+                                 relative_src_path, api_rules)
+
+                # Extract URLs and Emails
+                urls, urls_nf, emails_nf = url_n_email_extract(
+                    dat, relative_src_path)
+                url_list.extend(urls)
+                url_n_file.extend(urls_nf)
+                email_n_file.extend(emails_nf)
         urls_list = list(set(url_list))
         # Domain Extraction and Malware Check
         logger.info('Performing Malware Check on extracted Domains')
@@ -70,7 +76,6 @@ def ios_source_analysis(src):
             'urlnfile': url_n_file,
             'domains': domains,
             'emailnfile': email_n_file,
-            'source_type': source_type,
         }
         return code_analysis_dict
 
