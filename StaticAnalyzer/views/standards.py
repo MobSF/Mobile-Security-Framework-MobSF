07bb5786768005bee3e55386505dd9e4bf28089c
Mariusz Brzeski <mariusz.brzeski@netguru.com>
2020-03-11 10:11:03 +0100

    add CWE-310 standard - Cryptographic Issues

--- StaticAnalyzer/views/standards.py
+++ StaticAnalyzer/views/standards.py
@@ -129,6 +129,7 @@ CWE = {
     'CWE-250': 'CWE-250 - Execution with Unnecessary Privileges',
     'CWE-276': 'CWE-276 - Incorrect Default Permissions',
     'CWE-295': 'CWE-295 - Improper Certificate Validation',
+    'CWE-310': 'CWE-310 - Cryptographic Issues',
     'CWE-311': 'CWE-311 - Missing Encryption of Sensitive Data',
     'CWE-312': 'CWE-312 - Cleartext Storage of Sensitive Information',
     'CWE-327': 'CWE-327 - Use of a Broken or Risky Cryptographic Algorithm',
