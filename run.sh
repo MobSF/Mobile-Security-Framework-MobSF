dca108d192701303c16d2c6c3aba7fddb170b6d1
superpoussin22 <vincent.nadal@orange.fr>
2020-06-01 17:23:33 +0200

    ip and port as variable

--- run.sh
+++ run.sh
@@ -2,4 +2,22 @@
 # Dev Server
 #. venv/bin/activate && python manage.py runserver
 # Prod Server
-. venv/bin/activate && gunicorn -b 127.0.0.1:8000 MobSF.wsgi:application --workers=1 --threads=10 --timeout=1800
+var="$1"
+if [ ! -z "$var" ]; then
+    IP=$(echo $var | awk -F':' '{print $1}')
+    if echo "$IP" | { IFS=. read a b c d e;
+    test "$a" -ge 0 && test "$a" -le 255 &&
+    test "$b" -ge 0 && test "$b" -le 255 &&
+    test "$c" -ge 0 && test "$c" -le 255 &&
+    test "$d" -ge 0 && test "$d" -le 255 &&
+    test -z "$e"; }; then
+        PORT=$(echo $var | awk -F':' '{print $2}')
+       else
+       	echo 'Bad IP !'
+        exit 1	
+    fi 	
+ else
+    IP='0.0.0.0'
+    PORT='8000'
+fi	 
+. venv/bin/activate && gunicorn -b ${IP}:${PORT} MobSF.wsgi:application --workers=1 --threads=10 --timeout=1800
