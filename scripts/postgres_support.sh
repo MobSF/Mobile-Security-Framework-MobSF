edb38b693b36c10ef1f2ed511d2c897348c432fd
DE BOCK Arnaud <arnaud.debock@orange.com>
2020-07-28 13:09:21 +0200

    Add Postgresql support

--- scripts/postgres_support.sh
+++ scripts/postgres_support.sh
@@ -8,6 +8,9 @@ if [ "$POSTGRES" == True ]; then
  sed -i '/# Sqlite3 suport/,/# End Sqlite3 support/d' ../MobSF/settings.py && \
  sed -i '/# Postgres DB - Install psycopg2/,/"""/d' ../MobSF/settings.py && \
  sed -i '/# End Postgres support/,/"""/d' ../MobSF/settings.py && \
- sed -i "s/'PASSWORD': '',/'PASSWORD': 'password',/" ../MobSF/settings.py && \
- sed -i "s/'HOST': 'localhost',/'HOST': 'postgres',/" ../MobSF/settings.py
+# sed -i "s/'USER': '',/'postgres': '$POSTGRES_USER',/" ../MobSF/settings.py && \
+# sed -i "s/'PASSWORD': '',/'PASSWORD': '$POSTGRES_PASSWORD',/" ../MobSF/settings.py && \
+# sed -i "s/'NAME': '',/'mobsf': '$POSTGRES_DB',/" ../MobSF/settings.py && \
+# sed -i "s/'HOST': 'localhost',/'HOST': '$POSTGRES_HOST',/" ../MobSF/settings.py
+ cat ../MobSF/settings.py
 fi
