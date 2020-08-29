7e70ec3092a02c3038ef51a72475cc579128f1c7
Ajin Abraham <ajin25@gmail.com>
2020-08-29 15:33:11 -0400

    3.1.4

--- MobSF/settings.py
+++ MobSF/settings.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-MOBSF_VER = 'v3.1.3 Beta'
+MOBSF_VER = 'v3.1.4 Beta'
 
 BANNER = """
   __  __       _    ____  _____       _____  _ 
