b6bb8ea78866c1108564f9d8106ddc04de7a55a5
superpoussin22 <vincent.nadal@orange.fr>
2020-10-28 10:10:04 +0100

    add frida 14.0.5

--- DynamicAnalyzer/views/android/environment.py
+++ DynamicAnalyzer/views/android/environment.py
@@ -31,7 +31,7 @@ from StaticAnalyzer.models import StaticAnalyzerAndroid
 
 logger = logging.getLogger(__name__)
 ANDROID_API_SUPPORTED = 29
-FRIDA_VERSION = '12.11.17'
+FRIDA_VERSION = '14.0.5'
 
 
 class Environment:
