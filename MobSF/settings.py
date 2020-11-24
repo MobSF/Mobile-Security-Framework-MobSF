1b2f09b6b21c2e92000ee13cf23aaee640c57658
Ming <6680424qwer@163.com>
2020-11-24 10:17:41 +0800

    website display error in debug mode

--- MobSF/settings.py
+++ MobSF/settings.py
@@ -204,7 +204,7 @@ TEMPLATES = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # 256MB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 268435456
