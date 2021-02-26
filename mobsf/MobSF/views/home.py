2dcbd339c163642e8b39c5ebc82aa96b0cad5177
Ajin Abraham <ajin25@gmail.com>
2021-02-26 14:32:00 -0800

    Test fix and Frida update (#1685)

--- mobsf/MobSF/views/home.py
+++ mobsf/MobSF/views/home.py
@@ -225,8 +225,8 @@ def search(request):
         db_obj = RecentScansDB.objects.filter(MD5=md5)
         if db_obj.exists():
             e = db_obj[0]
-            url = (f'/{e.ANALYZER }/?name={e.FILE_NAME}&amp;'
-                   f'checksum={e.MD5}&amp;type={e.SCAN_TYPE}')
+            url = (f'/{e.ANALYZER }/?name={e.FILE_NAME}&'
+                   f'checksum={e.MD5}&type={e.SCAN_TYPE}')
             return HttpResponseRedirect(url)
         else:
             return HttpResponseRedirect('/not_found/')
