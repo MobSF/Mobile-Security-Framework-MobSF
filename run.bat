03adcf58a81b8c7ae65d219b2d1eab200594a748
Ajin Abraham <ajin25@gmail.com>
2021-04-19 19:43:32 -0700

    Windows Setup Script QA

--- run.bat
+++ run.bat
@@ -4,6 +4,15 @@ if [%1]==[] goto usage
 SET conf=%1
 goto :run
 :usage
-SET conf="0.0.0.0:8000"
+SET conf=0.0.0.0:8000
 :run
-.\venv\Scripts\activate && waitress-serve --listen=%conf% --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
+set server=.\venv\Scripts\waitress-serve.exe
+if exist %server% (
+  echo Running MobSF on %conf%
+  %server% --listen=%conf% --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
+  exit /b 0
+) else (
+  echo [ERROR] Incomplete setup. Please ensure that setup.bat completes without any errors.
+  pause
+  exit /b
+)
