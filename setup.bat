03cd2c65c982a6acbdf7542f374ec977e0fe25ed
superpoussin22 <vincent.nadal@orange.fr>
2021-11-29 09:07:50 +0100

    1st try for windows jadx installation

--- setup.bat
+++ setup.bat
@@ -65,7 +65,12 @@ where python >nul 2>&1 && (
   echo [INSTALL] Installing Requirements
   %venv% -m pip install --no-cache-dir wheel
   %venv% -m pip install --no-cache-dir --use-deprecated=legacy-resolver -r requirements.txt
-  
+
+  echo [INSTALL] Installing Jadx
+  %venv% -m gh-release-install 'skylot/jadx' '\mobsf\StaticAnalyzer\tools\jadx.zip' --version-file 'mobsf\StaticAnalyzer\tools\jadx-{version}.ver' 
+  unzip -qq -d mobsf\StaticAnalyzer\tools\jadx\ mobsf\StaticAnalyzer\tools\jadx.zip 
+  del /f mobsf\StaticAnalyzer\tools\jadx.zip >nul 2>&1
+
   echo [INSTALL] Clean Up
   call scripts/clean.bat y
 
