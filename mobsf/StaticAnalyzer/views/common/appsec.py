88d0d71dfbe4e624be7c5d749cf7b882719c92cd
Ajin.Abraham <ajin.abraham@chime.com>
2022-01-22 22:55:05 -0800

    code qa

--- mobsf/StaticAnalyzer/views/common/appsec.py
+++ mobsf/StaticAnalyzer/views/common/appsec.py
@@ -4,10 +4,8 @@ Shared Functions.
 
 AppSec Dashboard
 """
-import json
 import logging
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from mobsf.MobSF import settings
@@ -24,6 +22,95 @@ from mobsf.StaticAnalyzer.views.ios.db_interaction import (
     get_context_from_db_entry as idb)
 
 logger = logging.getLogger(__name__)
+
+
+def common_fields(findings, data):
+    """Common Fields for Android and iOS."""
+    # Code Analysis
+    for cd in data['code_analysis'].values():
+        if cd['metadata']['severity'] == 'good':
+            sev = 'secure'
+        else:
+            sev = cd['metadata']['severity']
+        desc = cd['metadata']['description']
+        ref = cd['metadata'].get('ref', '')
+        findings[sev].append({
+            'title': cd['metadata']['description'],
+            'description': f'{desc}\n{ref}',
+            'section': 'code',
+        })
+    # Malicious Domains
+    for domain, value in data['domains'].items():
+        if value['bad'] == 'yes':
+            findings['high'].append({
+                'title': f'Malicious domain found - {domain}',
+                'description': str(value['geolocation']),
+                'section': 'domains',
+            })
+    # Firebase
+    for fb in data['firebase_urls']:
+        if fb['open']:
+            fdb = fb['url']
+            findings['high'].append({
+                'title': 'Firebase DB is exposed publicly.',
+                'description': (
+                    f'The Firebase database at {fdb} is exposed'
+                    ' to internet without any authentication'),
+                'section': 'firebase',
+            })
+    # Trackers
+    if 'trackers' in data['trackers']:
+        findings['total_trackers'] = data['trackers']['total_trackers']
+        t = len(data['trackers']['trackers'])
+        findings['trackers'] = t
+        if t > 4:
+            findings['high'].append({
+                'title': 'Application contains Privacy Trackers',
+                'description': (
+                    f'This app has more than {t} privacy trackers.'
+                    ' Trackers can track device or users and '
+                    'are privacy concerns for end users.'),
+                'section': 'trackers',
+            })
+        elif t > 0:
+            findings['warning'].append({
+                'title': 'Application contains Privacy Trackers',
+                'description': (
+                    f'This app has {t} privacy trackers.'
+                    ' Trackers can track device or users and '
+                    'are privacy concerns for end users.'),
+                'section': 'trackers',
+            })
+        else:
+            findings['secure'].append({
+                'title': 'This application has no privacy trackers',
+                'description': (
+                    'This application does not include any user '
+                    'or device trackers. Unable to find trackers '
+                    'during static analysis.'),
+                'section': 'trackers',
+            })
+    # Possible Hardcoded Secrets
+    secrets = data['secrets']
+    if len(secrets) > 1:
+        sec = '\n'.join(secrets)
+        findings['warning'].append({
+            'title': 'This app may contain hardcoded secrets',
+            'description': (
+                'The following secrets were identified from the app. '
+                'Ensure that these are not secrets or private information.\n'
+                f'{sec}'),
+            'section': 'secrets',
+        })
+    high = len(findings.get('high'))
+    warn = len(findings.get('warning'))
+    sec = len(findings.get('secure'))
+    total = high + warn + sec
+    findings['security_score'] = int(100 - (
+        ((high * 1) + (warn * .5) - (sec * .2)) / total) * 100)
+    findings['app_name'] = data.get('app_name', '')
+    findings['file_name'] = data.get('file_name', '')
+    findings['hash'] = data['md5']
 
 
 def get_android_dashboard(context, from_ctx=False):
@@ -80,92 +167,8 @@ def get_android_dashboard(context, from_ctx=False):
             'description': desc,
             'section': 'manifest',
         })
-    # Code Analysis
-    for cd in data['code_analysis'].values():
-        if cd['metadata']['severity'] == 'good':
-            sev = 'secure'
-        else:
-            sev = cd['metadata']['severity']
-        desc = cd['metadata']['description']
-        ref = cd['metadata'].get('ref', '')
-        findings[sev].append({
-            'title': cd['metadata']['description'],
-            'description': f'{desc}\n{ref}',
-            'section': 'code',
-        })
-    # Domains
-    for domain, value in data['domains'].items():
-        if value['bad'] == 'yes':
-            findings['high'].append({
-                'title': f'Malicious domain found - {domain}',
-                'description': str(value['geolocation']),
-                'section': 'domains',
-            })
-    # Firebase
-    for fb in data['firebase_urls']:
-        if fb['open']:
-            fdb = fb['url']
-            findings['high'].append({
-                'title': 'Firebase DB is exposed publicly.',
-                'description': (
-                    f'The Firebase database at {fdb} is exposed'
-                    ' to internet without any authentication'),
-                'section': 'firebase',
-            })
-    # Trackers
-    if 'trackers' in data['trackers']:
-        findings['total_trackers'] = data['trackers']['total_trackers']
-        t = len(data['trackers']['trackers'])
-        findings['trackers'] = t
-        if t > 4:
-            findings['high'].append({
-                'title': 'Application contains Privacy Trackers',
-                'description': (
-                    f'This app has more than {t} privacy trackers.'
-                    ' Trackers can track device or users and can '
-                    'are privacy concerns for end users.'),
-                'section': 'trackers',
-            })
-        elif t > 0:
-            findings['warning'].append({
-                'title': 'Application contains Privacy Trackers',
-                'description': (
-                    f'This app has {t} privacy trackers.'
-                    ' Trackers can track device or users and can '
-                    'are privacy concerns for end users.'),
-                'section': 'trackers',
-            })
-        else:
-            findings['secure'].append({
-                'title': 'This application has no privacy trackers',
-                'description': (
-                    'Unable to find privacy trackers during static analysis.'
-                    ' This app does not include any trackers.'),
-                'section': 'trackers',
-            })
-    # Possible Hardcoded Secrets
-    secrets = data['secrets']
-    if len(secrets) > 1:
-        sec = '\n'.join(secrets)
-        findings['warning'].append({
-            'title': 'This app may contain hardcoded secrets',
-            'description': (
-                'The following secrets were identified from the app. '
-                'Ensure that these are not secrets or private information.\n'
-                f'{sec}'),
-            'section': 'secrets',
-        })
-    high = len(findings.get('high'))
-    warn = len(findings.get('warning'))
-    sec = len(findings.get('secure'))
-    total = high + warn + sec
-    findings['security_score'] = int(100 - (
-        ((high * 1) + (warn * .5) - (sec * .2)) / total) * 100)
-    findings['app_name'] = data.get('app_name', '')
-    findings['file_name'] = data.get('file_name', '')
+    common_fields(findings, data)
     findings['version_name'] = data.get('version_name', '')
-    findings['icon_found'] = data['icon_found']
-    findings['hash'] = data['md5']
     return findings
 
 
@@ -249,92 +252,8 @@ def get_ios_dashboard(context, from_ctx=False):
                 'description': symbol['description'],
                 'section': 'macho',
             })
-    # Code Analysis
-    for cd in data['code_analysis'].values():
-        if cd['metadata']['severity'] == 'good':
-            sev = 'secure'
-        else:
-            sev = cd['metadata']['severity']
-        desc = cd['metadata']['description']
-        ref = cd['metadata'].get('ref', '')
-        findings[sev].append({
-            'title': cd['metadata']['description'],
-            'description': f'{desc}\n{ref}',
-            'section': 'code',
-        })
-    # Domains
-    for domain, value in data['domains'].items():
-        if value['bad'] == 'yes':
-            findings['high'].append({
-                'title': f'Malicious domain found - {domain}',
-                'description': str(value['geolocation']),
-                'section': 'domains',
-            })
-    # Firebase
-    for fb in data['firebase_urls']:
-        if fb['open']:
-            fdb = fb['url']
-            findings['high'].append({
-                'title': 'Firebase DB is exposed publicly.',
-                'description': (
-                    f'The Firebase database at {fdb} is exposed'
-                    ' to internet without any authentication'),
-                'section': 'firebase',
-            })
-    # Trackers
-    if 'trackers' in data['trackers']:
-        findings['total_trackers'] = data['trackers']['total_trackers']
-        t = len(data['trackers']['trackers'])
-        findings['trackers'] = t
-        if t > 4:
-            findings['high'].append({
-                'title': 'Application contains Privacy Trackers',
-                'description': (
-                    f'This app has more than {t} privacy trackers.'
-                    ' Trackers can track device or users and can '
-                    'are privacy concerns for end users.'),
-                'section': 'trackers',
-            })
-        elif t > 0:
-            findings['warning'].append({
-                'title': 'Application contains Privacy Trackers',
-                'description': (
-                    f'This app has {t} privacy trackers.'
-                    ' Trackers can track device or users and can '
-                    'are privacy concerns for end users.'),
-                'section': 'trackers',
-            })
-        else:
-            findings['secure'].append({
-                'title': 'This application has no privacy trackers',
-                'description': (
-                    'Unable to find privacy trackers during static analysis.'
-                    ' This app does not include any trackers.'),
-                'section': 'trackers',
-            })
-    # Possible Hardcoded Secrets
-    secrets = data['secrets']
-    if len(secrets) > 1:
-        sec = '\n'.join(secrets)
-        findings['warning'].append({
-            'title': 'This app may contain hardcoded secrets',
-            'description': (
-                'The following secrets were identified from the app. '
-                'Ensure that these are not secrets or private information.\n'
-                f'{sec}'),
-            'section': 'secrets',
-        })
-    high = len(findings.get('high'))
-    warn = len(findings.get('warning'))
-    sec = len(findings.get('secure'))
-    total = high + warn + sec
-    findings['security_score'] = int(100 - (
-        ((high * 1) + (warn * .5) - (sec * .2)) / total) * 100)
-    findings['app_name'] = data.get('app_name', '')
-    findings['file_name'] = data.get('file_name', '')
+    common_fields(findings, data)
     findings['version_name'] = data.get('app_version', '')
-    findings['icon_found'] = data['icon_found']  # Does not exists for iOS
-    findings['hash'] = data['md5']
     return findings
 
 
@@ -353,10 +272,8 @@ def appsec_dashboard(request, checksum, api=False):
             if api:
                 return {'not_found': 'Report not found or supported'}
             else:
-                return HttpResponse(
-                    json.dumps({'not_found': 'Report not found or supported'}),
-                    content_type='application/json; charset=utf-8',
-                    status=500)
+                msg = 'Report not found or supported'
+                return print_n_send_error_response(request, msg)
         context['version'] = settings.MOBSF_VER
         context['title'] = 'AppSec Scorecard'
         if api:
