c79172849cde7a31723a4cf34fc4ff022a811d59
Brice Williams <brice.williams-ext@jci.com>
2022-02-12 17:33:14 -0800

    Fixed bug that forced database name of mobsf

--- mobsf/MobSF/settings.py
+++ mobsf/MobSF/settings.py
@@ -139,7 +139,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mobsf',
+        'NAME': os.environ['POSTGRES_DB'],
         'USER': os.environ['POSTGRES_USER'],
         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': os.environ['POSTGRES_HOST'],
