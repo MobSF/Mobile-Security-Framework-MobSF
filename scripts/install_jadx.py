db934cd5224fab3e15c73dabd2612467c4e4ca54
superpoussin22 <vincent.nadal@orange.fr>
2022-03-04 15:48:13 +0100

    solve linter report

--- scripts/install_jadx.py
+++ scripts/install_jadx.py
@@ -1,26 +1,29 @@
-import json
 import logging
 import os 
-import requests
 import shutil
 import urllib.request
 
+import requests
+
+
 logger = logging.getLogger(__name__)
+
+JADX_URL='https://api.github.com/repos/skylot/jadx/releases/latest'
 
 print('Installing Jadx')
 logger.info('Installing Jadx')
-response = requests.get("https://api.github.com/repos/skylot/jadx/releases/latest")
-url = response.json()["assets"][0]["browser_download_url"]
+response = requests.get(JADX_URL)
+url = response.json()['assets'][0]['browser_download_url']
 try:
     urllib.request.urlretrieve(url, './jadx.zip')
-except:
-    logger.error( "Failed to download '{}'".format(url))
+except Exception:
+    logger.error('Failed to download Jadx')
 if os.path.exists('./mobsf/StaticAnalyzer/tools/jadx/'):
     shutil.rmtree('./mobsf/StaticAnalyzer/tools/jadx/')
 os.makedirs('./mobsf/StaticAnalyzer/tools/jadx/')
 if os.path.exists('./mobsf/StaticAnalyzer/tools/jadx/'):
     print('Unpacking Jadx')
-    shutil.unpack_archive('./jadx.zip','./mobsf/StaticAnalyzer/tools/jadx/' )
+    shutil.unpack_archive('./jadx.zip','./mobsf/StaticAnalyzer/tools/jadx/')
 os.remove('./jadx.zip')
 if os.path.exists('./mobsf/StaticAnalyzer/tools/jadx/bin/jadx'):
     os.chmod('./mobsf/StaticAnalyzer/tools/jadx/bin/jadx', 0o777)
