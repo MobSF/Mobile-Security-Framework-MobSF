cec5bd0bccc77f4580a0f42545e1a641a6852c12
talson <44098012+ripp3rdoc@users.noreply.github.com>
2022-05-29 20:24:06 +0300

    Fixed bugs

--- run.bat
+++ run.bat
@@ -1,19 +1,2 @@
 @echo off
-
-if [%1]==[] goto usage
-SET conf=%1
-goto :run
-:usage
-SET conf=0.0.0.0:8000
-:run
-set venv=.\venv\Scripts\activate
-set server=.\venv\Scripts\waitress-serve.exe
-if exist %server% (
-  echo Running MobSF on %conf%
-  %venv%
-  %server% --listen=%conf% --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
-) else (
-  echo [ERROR] Incomplete setup. Please ensure that setup.bat completes without any errors.
-  pause
-  exit /b
-)
+waitress-serve.exe --listen=0.0.0.0:8000 --threads=10 --channel-timeout=3600 mobsf.MobSF.wsgi:application
