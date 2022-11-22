cb8530b3d4ed5a1ccc106187dd2dd670303063c9
Ajin Abraham <ajin25@gmail.com>
2022-11-22 13:43:47 -0800

    update windows setup

--- scripts/clean.bat
+++ scripts/clean.bat
@@ -1,6 +1,7 @@
 @echo off 
 echo =======================MobSF Clean Script for Windows=======================
 echo Running this script will delete the Scan database, all files uploaded and generated.
+SET mobsf_home="%userprofile%\.MobSF"
 SET mypath=%~dp0
 echo %mypath:~0,-1%
 IF "%~1"=="y" (
@@ -24,6 +25,8 @@ del /f "mobsf\secret" >nul 2>&1
 echo Deleting Previous setup files
 rmdir "%UserProfile%\MobSF" /q /s >nul 2>&1
 del /f "mobsf\setup_done.txt" >nul 2>&1
+echo Deleting MobSF data directory: %mobsf_home%
+del /f "%mobsf_home%" /q /s >nul 2>&1
 echo Done
 ) ELSE ( 
 echo Please run script from mobsf.MobSF directory
