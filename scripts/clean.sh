7e174e1264648eb38dc0eca320ed5c949f5ec4f2
Ajin.Abraham <ajin.abraham@chime.com>
2022-11-22 13:13:28 -0800

    Setup fixes

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,8 @@ echo '=======================MobSF Clean Script for Unix======================='
 echo 'Running this script will delete the Scan database, all files uploaded and generated.'
 
 script_path=$(dirname $0)
+mobsf_home="$HOME/.MobSF"
+
 if [ "$script_path" != "scripts" ] && [ "$script_path" != "./scripts" ]; then
     echo 'Please run script from mobsf.MobSF directory '
     echo './scripts/clean.sh '
@@ -19,25 +21,27 @@ fi
 echo 
 if [[ $VAL =~ ^[Yy]$ ]]
 then
-	echo 'Deleting all Uploads'
+	echo 'Deleting all uploads'
 	rm -rf ./mobsf/uploads/*
-	echo 'Deleting all Downloads'
+	echo 'Deleting all downloads'
 	rm -rf ./mobsf/downloads/*
-	echo 'Deleting Static Analyzer Migrations'
+	echo 'Deleting Static Analyzer migrations'
 	rm -rf ./mobsf/StaticAnalyzer/migrations/*
-	echo 'Deleting Dynamic Analyzer Migrations'
+	echo 'Deleting Dynamic Analyzer migrations'
 	rm -rf ./mobsf/DynamicAnalyzer/migrations/*
-	echo 'Deleting MobSF Migrations'
+	echo 'Deleting MobSF migrations'
 	rm -rf ./mobsf/MobSF/migrations/*
-	echo 'Deleting python byte code files'
+	echo 'Deleting Python byte code files'
         find ./ -name "*.pyc" -exec rm -rf {} \;
         find ./ | grep -E "(__pycache__|\.pyo$)" | xargs rm -rf
         echo 'Deleting temp and log files'
 	rm -rf ./mobsf/debug.log
 	rm -rf ./classes*
-	echo 'Deleting DB'
+	echo 'Deleting Scan database'
 	rm -rf ./mobsf/db.sqlite3
-	echo 'Deleting Secret File'
+	echo 'Deleting Secret file'
 	rm -rf ./mobsf/secret
+	echo "Deleting MobSF data directory: $mobsf_home"
+	rm -rf $mobsf_home
 	echo 'Done'
 fi
