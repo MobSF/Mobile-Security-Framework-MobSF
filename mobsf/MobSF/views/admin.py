04ac59cd6aa10fe63a8579414ed3185a4344b891
brice-syslogic <65510350+brice-syslogic@users.noreply.github.com>
2023-03-20 14:36:35 -0700

    Basic underlying support for creating an API key

---
+++ mobsf/MobSF/views/admin.py
@@ -0,0 +1,91 @@
+# -*- coding: utf_8 -*-
+import base64
+import hashlib
+import logging
+import os
+import traceback as tb
+
+from django.conf import settings
+from django.shortcuts import render
+
+from mobsf.StaticAnalyzer.models import ApiKeys
+from mobsf.MobSF.utils import (
+    error_response,
+    is_admin,
+    sso_email,
+    tz,
+    utcnow,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def create_api_key(description, email, role, expire_date):
+    """Create new APIKeys record."""
+    random_bytes = os.urandom(32)
+    api_key = base64.b64encode(random_bytes).replace('=', '')
+    key_hash = hashlib.sha256(api_key.encode('utf-8')).hexdigest()
+    new_db_obj = ApiKeys(
+        KEY_HASH=key_hash,
+        KEY_PREFIX=api_key[0:5],
+        DESCRIPTION=description,
+        EMAIL=email,
+        ROLE=role,
+        CREATE_DATE=utcnow(),
+        EXPIRE_DATE=expire_date
+    )
+    new_db_obj.save()
+    logger.info('New API key %s... created by %s', api_key[0:5], sso_email())
+    return (api_key, new_db_obj)
+
+
+def get_api_keys():
+    return ApiKeys.objects.all().values().order_by('EXPIRE_DATE')
+
+
+def admin_view(request):
+    if (not is_admin(request)):
+        return error_response(request, 'Unauthorized')
+    
+    entries = []
+    entry = {"id": 1, "description":  "Origis Consulting", "email": "bwilliams@syslogicinc.com","role": "Full Access", "api_key": "MTIzN…", "created_on": "2/16/2023", "expires_on": "2/17/2024"}
+    entries.append(entry)
+    entry = {"id": 2,"description":  "CapGemini", "email": "eransom@syslogicinc.com","role": "Upload Only", "api_key": "AfVyZ…", "created_on": "2/1/2023", "expires_on": "2/2/2024"}
+    entries.append(entry)
+    entry = {"id": 3,"description":  "SysLogic", "email": "wmccardell@syslogicinc.com","role": "Read Only", "api_key": "lmE…", "created_on": "2/28/2023", "expires_on": "3/1/2024"}
+    entries.append(entry)
+
+    context = {
+        'title': 'Admin Settings',
+        'entries': entries, # Should be able to replace this with get_api_keys() when ready
+        'version': settings.MOBSF_VER,
+        'tenant_static': settings.TENANT_STATIC_URL,
+    }
+    template = 'general/admin.html'
+    return render(request, template, context)
+
+
+def create_api_key_post(request):    
+    try:
+        if (not is_admin(request)):
+            return error_response(request, 'Unauthorized')
+    
+        default_expire_date = utcnow()
+        default_expire_date.replace(year=default_expire_date.year + 1)
+        # Validate input parameters
+        description = request.POST.get('description', '')
+        email = request.POST.get('email', sso_email(request))
+        role = request.POST.get('role', '') # Need to set default role
+        expire_date = request.POST.get('expire_date', default_expire_date)
+
+        if not description:
+            return error_response(request, 'Missing parameter: description')
+        
+        return create_api_key(description, email, tz(role), expire_date)
+
+    except Exception as exp:
+        exmsg = ''.join(tb.format_exception(None, exp, exp.__traceback__))
+        logger.error(exmsg)
+        msg = str(exp)
+        exp_doc = exp.__doc__
+        return error_response(request, msg, False, exp_doc)
