6963d3190cc73982e051ef5332ec28ce930fbada
Karmaz <51202595+Karmaz95@users.noreply.github.com>
2023-08-11 16:35:31 +0200

    Update macho_analysis.py
    Added missing brackets.

--- mobsf/StaticAnalyzer/views/ios/macho_analysis.py
+++ mobsf/StaticAnalyzer/views/ios/macho_analysis.py
@@ -226,7 +226,7 @@ class Checksec:
     def is_symbols_stripped(self):
         filter_symbols = ['radr://5614542', '__mh_execute_header']
         for i in self.macho.symbols:
-            if (i.type & 0xe0) > 0 or (i.type in [0x0e,0x1e,0x0f]) and i.name.lower().strip() not in filter_symbols:
+            if ((i.type & 0xe0) > 0 or (i.type in [0x0e,0x1e,0x0f])) and i.name.lower().strip() not in filter_symbols:
                 return False
         return True
 
