8ce079ff8685a7c3099e4908a2d16e2818aa4dcb
Bryan Wong <bryanwonghg2004@gmail.com>
2023-08-14 09:03:36 +0800

    lel

--- mobsf/DynamicAnalyzer/tools/frida_scripts/others/file_writable_check.js
+++ mobsf/DynamicAnalyzer/tools/frida_scripts/others/file_writable_check.js
@@ -32,47 +32,43 @@ Java.perform(function() {
     };
 
 
-    // Hook write(byte[] b) method
-    FileOutputStream.write.overload('[B').implementation = function(b) {
-        var filePath = fileMap[this.hashCode()];  // Get the file path
+// Hook write(byte[] b) method
+FileOutputStream.write.overload('[B').implementation = function(b) {
+    var filePath = fileMap[this.hashCode()];  // Get the file path
 
-        // Convert the entire buffer to a string
-        var buffer = Java.array('byte', b);
-        var str = StringCls.$new(buffer, 0, b.length, "UTF-8");
+    // Convert the byte array to a human-readable string
+    var byteArray = Java.array('byte', b);
+    var humanReadableString = StringCls.$new(byteArray, "UTF-8");
 
-        send('Warning: Data(bytes) being written: ' + str + ' at filepath: ' + filePath);
-        send(b);
-        
-        // Check for large write
-        if (buffer.length > 1024 * 1024) {
-            send('WARNING: Large write operation: ' + buffer.length + ' bytes');
-        }
-        else {
-            send('No large write operations detected')
-        }
-        return FileOutputStream.write.overload('[B').call(this, buffer);
-    };
+    send('Warning: Data(bytes) being written in human-readable form: ' + humanReadableString + ' at filepath: ' + filePath);
 
-    // Hook write(byte[] b, int off, int len) method
-    FileOutputStream.write.overload('[B', 'int', 'int').implementation = function(b, off, len) {
-        var filePath = fileMap[this.hashCode()];  // Get the file path
+    // Check for large write
+    if (b.length > 1024 * 1024) {
+        send('WARNING: Large write operation: ' + b.length + ' bytes');
+    } else {
+        send('No large write operations detected')
+    }
+    return FileOutputStream.write.overload('[B').call(this, b);
+};
 
-        // Convert the part of the buffer to a string
-        var buffer = Java.array('byte', b);
-        var str = StringCls.$new(buffer, off, len, "UTF-8");
-    
-        send('Warning: Data(bytes and int) being written: ' + str + ' at filepath: ' + filePath);
-        
-        // Check for large write
-        if (len > 1024 * 1024) {
-            send('WARNING: Large write operation: ' + len + ' bytes');
-        }
-        else {
-            send('No large write operations detected')
-        }
-        return FileOutputStream.write.overload('[B', 'int', 'int').call(this, b, off, len);
-    };
+// Hook write(byte[] b, int off, int len) method
+FileOutputStream.write.overload('[B', 'int', 'int').implementation = function(b, off, len) {
+    var filePath = fileMap[this.hashCode()];  // Get the file path
 
+    // Convert the part of the byte array to a human-readable string
+    var partByteArray = Java.array('byte', Array.from(b).slice(off, off + len));
+    var humanReadableString = StringCls.$new(partByteArray, "UTF-8");
+
+    send('Warning: Data(bytes and int) being written in human-readable form: ' + humanReadableString + ' at filepath: ' + filePath);
+
+    // Check for large write
+    if (len > 1024 * 1024) {
+        send('WARNING: Large write operation: ' + len + ' bytes');
+    } else {
+        send('No large write operations detected')
+    }
+    return FileOutputStream.write.overload('[B', 'int', 'int').call(this, b, off, len);
+};
     File.setWritable.overload('boolean', 'boolean').implementation = function(writable, ownerOnly) {
         // If the file is being set as writable, warn the user
         if (writable) {
