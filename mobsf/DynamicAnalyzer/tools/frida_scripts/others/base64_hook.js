a990d0f7851519927e0a7ce189005e783a66ae47
Isaac Koh <isaackoh88@gmail.com>
2023-10-12 17:12:36 +0800

    Added. Stack trace configuration for Base64 operation hooks.

--- mobsf/DynamicAnalyzer/tools/frida_scripts/others/base64_hook.js
+++ mobsf/DynamicAnalyzer/tools/frida_scripts/others/base64_hook.js
@@ -1,5 +1,13 @@
 Java.perform(function() {
 
+    // Config
+    var CONFIG = {
+        // if TRUE print stack trace
+        printStackTrace: false
+    };
+
+
+    // Declaring Android Objects
     var b64Def = Java.use('android.util.Base64');
 
     var b64DefEncode_2 = b64Def.encode.overload('[B', 'int');
@@ -15,40 +23,44 @@ Java.perform(function() {
 
     // Base64 Encoding Hooks
     b64DefEncode_2.implementation = function(arr, flag) {
-        
         send("--------------------\n[Base64] Encode: " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefEncode_2.call(this, arr, flag);
     };
     
     b64DefEncode_3.implementation = function(arr, off, len, flag) {
-        
         send("--------------------\n[Base64] Encode: [" + off + "," + len + "] " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefEncode_3.call(this, arr, off, len, flag);
     };
 
 
     // Base64 Encode to String Hooks
     b64DefEncodeToString_2.implementation = function(arr, flag) {
-        
         send("--------------------\n[Base64] EncodeToString: " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefEncodeToString_2.call(this, arr, flag);
     };
 
     b64DefEncodeToString_3.implementation = function(arr, off, len, flag) {
-        
         send("--------------------\n[Base64] EncodeToString: [" + off + "," + len + "] " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefEncodeToString_3.call(this, arr, off, len, flag);
     };
 
@@ -56,27 +68,31 @@ Java.perform(function() {
     // Base64 Decoding Hooks
     b64DefDecode_1.implementation = function(str, flag) {
         send("--------------------\n[Base64] Decode: " + str);
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefDecode_1.call(this, str, flag);
     };
 
     b64DefDecode_2.implementation = function(arr, flag) {
-        
         send("--------------------\n[Base64] Decode: " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefDecode_2.call(this, arr, flag);
     };
 
     b64DefDecode_3.implementation = function(arr, off, len, flag) {
-        
         send("--------------------\n[Base64] Decode: [" + off + "," + len + "] " + JSON.stringify(arr));
-        Java.perform(function() {
-            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-        });
+        if (CONFIG.printStackTrace) {
+            Java.perform(function() {
+                send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+            });
+        }
         return b64DefDecode_3.call(this, arr, off, len, flag);
     };
 });
