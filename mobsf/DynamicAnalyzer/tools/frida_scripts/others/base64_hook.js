5e079dde2e10a352c30afe7768caec428b0d8bdd
Isaac Koh <isaackoh88@gmail.com>
2023-10-13 10:32:20 +0800

    Added. String conversion for Base64 Decode operation byte array output.

--- mobsf/DynamicAnalyzer/tools/frida_scripts/others/base64_hook.js
+++ mobsf/DynamicAnalyzer/tools/frida_scripts/others/base64_hook.js
@@ -72,7 +72,7 @@ Java.perform(function() {
     // Base64 Decoding Hooks
     b64DefDecode_1.implementation = function(str, flag) {
         var result = b64DefDecode_1.call(this, str, flag);
-        send("--------------------\n[Base64] Decode: " + str + "\n[Base64] Result: " + result);
+        send("--------------------\n[Base64] Decode: " + str + "\n[Base64] Result : " + result + " (" + decodeData2String(result) + ")");
         if (CONFIG.printStackTrace) {
             Java.perform(function() {
                 send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
@@ -83,7 +83,7 @@ Java.perform(function() {
 
     b64DefDecode_2.implementation = function(arr, flag) {
         var result = b64DefDecode_2.call(this, arr, flag);
-        send("--------------------\n[Base64] Decode: " + JSON.stringify(arr) + "\n[Base64] Result: " + result);
+        send("--------------------\n[Base64] Decode: " + JSON.stringify(arr) + "\n[Base64] Result : " + result + " (" + decodeData2String(result) + ")");
         if (CONFIG.printStackTrace) {
             Java.perform(function() {
                 send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
@@ -94,7 +94,7 @@ Java.perform(function() {
 
     b64DefDecode_3.implementation = function(arr, off, len, flag) {
         var result = b64DefDecode_3.call(this, arr, off, len, flag);
-        send("--------------------\n[Base64] Decode: [" + off + "," + len + "] " + JSON.stringify(arr) + "\n[Base64] Result: " + result);
+        send("--------------------\n[Base64] Decode: [" + off + "," + len + "] " + JSON.stringify(arr) + "\n[Base64] Result : " + result + " (" + decodeData2String(result) + ")");
         if (CONFIG.printStackTrace) {
             Java.perform(function() {
                 send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
@@ -102,4 +102,21 @@ Java.perform(function() {
         }
         return result;
     };
+
+
+    // Reformatting functions
+    function decodeData2String(data) {
+        if (data !== null) {
+            return byteArray2String(String(data).split(","));
+        }
+        return data;
+    }
+
+    function byteArray2String(array) {
+        var result = "";
+        for (var i = 0; i < array.length; i++) {
+            result += String.fromCharCode(parseInt(array[i]));
+        }
+        return result;
+    }
 });
