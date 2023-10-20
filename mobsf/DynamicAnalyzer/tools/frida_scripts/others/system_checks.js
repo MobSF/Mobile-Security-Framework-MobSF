34a02abe2dc74cd5ec2d2b4882884a2a508df4df
Isaac Koh <isaackoh88@gmail.com>
2023-10-20 13:52:13 +0800

    Added. Stack trace for system checks bypass script.

--- mobsf/DynamicAnalyzer/tools/frida_scripts/others/system_checks.js
+++ mobsf/DynamicAnalyzer/tools/frida_scripts/others/system_checks.js
@@ -3,7 +3,7 @@ Java.perform(function() {
     // Config
     var CONFIG = {
         // if TRUE print stack trace
-        printStackTrace: false
+        printStackTrace: true
     };
 
 
@@ -12,38 +12,55 @@ Java.perform(function() {
 
     telephonyManager.getImei.overloads[0].implementation = function() {
         send('--------------------\n[System Check] Application checking for device IMEI, returning: ' + '35253108' + '852947' + '2');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return('35253108' + '852947' + '2');
     };
     telephonyManager.getImei.overloads[1].implementation = function(slot) {
         send('--------------------\n[System Check] Application checking for device IMEI, returning: ' + '35253108' + '852947' + '2');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return('35253108' + '852947' + '2');
     };
 
     telephonyManager.getSimOperator.overload().implementation = function() {
         send('--------------------\n[System Check] getSimOperator call detected, returning:' + payl0ad);
+        if (CONFIG.printStackTrace) {stackTrace();}
         return payl0ad;
     };
     telephonyManager.getSimOperator.overload('int').implementation = function(sm) {
         send('--------------------\n[System Check] getSimOperator call detected, returning:' + payl0ad);
+        if (CONFIG.printStackTrace) {stackTrace();}
         return payl0ad;
     };
 
     telephonyManager.getSimOperatorName.overload().implementation = function() {
         send('--------------------\n[System Check] Application checking for SIM operator name, returning: ' + 'Vodafone');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return 'Vodafone';
     };
 
     telephonyManager.getNetworkCountryIso.overload().implementation = function() {
         send('--------------------\n[System Check] Application checking for Network Country ISO, returning: ' + 'deu');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return 'deu';
     };
     telephonyManager.getNetworkCountryIso.overload('int').implementation = function() {
         send('--------------------\n[System Check] Application checking for Network Country ISO, returning: ' + 'deu');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return 'deu';
     };
 
     telephonyManager.getSimCountryIso.overload().implementation = function() {
         send('--------------------\n[System Check] Application checking for SIM Country ISO, returning: ' + 'deu');
+        if (CONFIG.printStackTrace) {stackTrace();}
         return 'deu';
+    };
+
+
+
+    // Stack Trace Function
+    function stackTrace() {
+        Java.perform(function() {
+            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
+        });
     };
 });
