4bcc93ddf78a78d452b3c5a225be6818be2a19af
Ajin.Abraham <ajin.abraham@chime.com>
2024-01-09 16:33:18 -0800

    Fix lint

--- mobsf/StaticAnalyzer/views/android/android_manifest_desc.py
+++ mobsf/StaticAnalyzer/views/android/android_manifest_desc.py
@@ -162,8 +162,9 @@ MANIFEST_DESC = {
                         'be remediated by setting the launch mode attribute '
                         'to "singleInstance" or by setting an empty '
                         'taskAffinity (taskAffinity="") attribute. You can '
-                        'also update the target SDK version (%s) of the app to '
-                        '28 or higher to fix this issue at platform level.'),
+                        'also update the target SDK version (%s) of the '
+                        'app to 28 or higher to fix this issue at '
+                        'platform level.'),
         'name': ('Activity (%s) is vulnerable to Android '
                  'Task Hijacking/StrandHogg.'),
     },
@@ -180,8 +181,9 @@ MANIFEST_DESC = {
                         'be remediated by setting the launch mode attribute '
                         'to "singleInstance" and by setting an empty '
                         'taskAffinity (taskAffinity=""). You can also update '
-                        'the target SDK version (%s) of the app to 29 or higher '
-                        'to fix this issue at platform level.'),
+                        'the target SDK version (%s) of the app '
+                        'to 29 or higher to fix this issue at '
+                        'platform level.'),
         'name': 'Activity (%s) is vulnerable to StrandHogg 2.0',
     },
     'improper_provider_permission': {
