641f4430bc7a216165a2077b7a6f841a5f2f19b7
Ajin.Abraham <ajin.abraham@chime.com>
2024-03-09 13:19:05 -0800

    HOTFIX: target sdk bug

--- mobsf/StaticAnalyzer/views/android/manifest_analysis.py
+++ mobsf/StaticAnalyzer/views/android/manifest_analysis.py
@@ -310,9 +310,9 @@ def manifest_analysis(mfxml, ns, man_data_dic, src_type, app_dir):
                     try:
                         target_sdk = int(man_data_dic['target_sdk'])
                     except Exception:
-                        if man_an_dic.get('min_sdk'):
-                            target_sdk = int(man_an_dic['min_sdk'])
-                        else:
+                        try:
+                            target_sdk = int(man_data_dic['min_sdk'])
+                        except Exception:
                             target_sdk = ANDROID_8_0_LEVEL
                     if (target_sdk < ANDROID_9_0_LEVEL
                             and launchmode == 'singleTask'):
