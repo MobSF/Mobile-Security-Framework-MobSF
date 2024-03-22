4417da03ba8ec23adae91a5d2c78ec1287e56173
Ajin Abraham <ajin25@gmail.com>
2024-03-22 13:52:04 -0700

    poetry pyqt5 fixes (#2362)
    * poetry pyqt5 fixes
    
    * QA
    
    * fix
    
    * Cert analysis qa
    
    * QA
    
    * pin pyqt5

--- mobsf/StaticAnalyzer/views/android/cert_analysis.py
+++ mobsf/StaticAnalyzer/views/android/cert_analysis.py
@@ -8,9 +8,7 @@ import re
 import subprocess
 from pathlib import Path
 
-from androguard.util import get_certificate_name_string
-
-from asn1crypto import x509
+import asn1crypto
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
@@ -38,6 +36,62 @@ HASH_FUNCS = {
     'sha256': hashlib.sha256,
     'sha512': hashlib.sha512,
 }
+
+
+def get_certificate_name_string(name, short=False, delimiter=', '):
+    """
+    Function from androguard.
+
+    licensed  under the Apache License, Version 2.0.
+    https://github.com/androguard/androguard/blob/master/androguard/util.py
+    Format the Name type of a X509 Certificate in a human readable form.
+
+    :param name: Name object to return the DN from
+    :param short: Use short form (default: False)
+    :param delimiter: Delimiter string or character between
+        two parts (default: ', ')
+
+    :type name: dict or :class:`asn1crypto.x509.Name`
+    :type short: boolean
+    :type delimiter: str
+
+    :rtype: str
+    """
+    if isinstance(name, asn1crypto.x509.Name):
+        name = name.native
+
+    # For the shortform, we have a lookup table
+    # See RFC4514 for more details
+    _ = {
+        'business_category': ('businessCategory', 'businessCategory'),
+        'serial_number': ('serialNumber', 'serialNumber'),
+        'country_name': ('C', 'countryName'),
+        'postal_code': ('postalCode', 'postalCode'),
+        'state_or_province_name': ('ST', 'stateOrProvinceName'),
+        'locality_name': ('L', 'localityName'),
+        'street_address': ('street', 'streetAddress'),
+        'organization_name': ('O', 'organizationName'),
+        'organizational_unit_name': ('OU', 'organizationalUnitName'),
+        'title': ('title', 'title'),
+        'common_name': ('CN', 'commonName'),
+        'initials': ('initials', 'initials'),
+        'generation_qualifier': ('generationQualifier', 'generationQualifier'),
+        'surname': ('SN', 'surname'),
+        'given_name': ('GN', 'givenName'),
+        'name': ('name', 'name'),
+        'pseudonym': ('pseudonym', 'pseudonym'),
+        'dn_qualifier': ('dnQualifier', 'dnQualifier'),
+        'telephone_number': ('telephoneNumber', 'telephoneNumber'),
+        'email_address': ('E', 'emailAddress'),
+        'domain_component': ('DC', 'domainComponent'),
+        'name_distinguisher': ('nameDistinguisher', 'nameDistinguisher'),
+        'organization_identifier': (
+            'organizationIdentifier', 'organizationIdentifier'),
+    }
+    return delimiter.join(
+        ['{}={}'.format(
+            _.get(attr, (attr, attr))[0 if short else 1],
+            name[attr]) for attr in name])
 
 
 def get_hardcoded_cert_keystore(files):
@@ -70,7 +124,7 @@ def get_hardcoded_cert_keystore(files):
 def get_cert_details(data):
     """Get certificate details."""
     certlist = []
-    x509_cert = x509.Certificate.load(data)
+    x509_cert = asn1crypto.x509.Certificate.load(data)
     subject = get_certificate_name_string(x509_cert.subject, short=True)
     certlist.append(f'X.509 Subject: {subject}')
     certlist.append(f'Signature Algorithm: {x509_cert.signature_algo}')
