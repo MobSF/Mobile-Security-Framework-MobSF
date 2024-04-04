286a9052542785b4ca4780aed4635867f274e5c7
Ajin Abraham <ajin25@gmail.com>
2024-04-03 21:30:03 -0700

    [SECURITY] Fixes an SSRF vulnerability report from positive technologies

--- mobsf/MobSF/init.py
+++ mobsf/MobSF/init.py
@@ -10,7 +10,7 @@ from mobsf.install.windows.setup import windows_config_local
 
 logger = logging.getLogger(__name__)
 
-VERSION = '3.9.7'
+VERSION = '3.9.8'
 BANNER = """
   __  __       _    ____  _____       _____ ___  
  |  \/  | ___ | |__/ ___||  ___|_   _|___ // _ \ 
