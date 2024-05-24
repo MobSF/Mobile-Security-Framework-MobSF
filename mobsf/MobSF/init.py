8b97723817d6101ee765052f91ab99de4d1dd018
Ajin Abraham <ajin.abraham@chime.com>
2024-05-23 23:32:13 -0700

    HOTFIX: Fix docker run errors

--- mobsf/MobSF/init.py
+++ mobsf/MobSF/init.py
@@ -92,8 +92,6 @@ def migrate(base_dir):
     try:
         django_operation(['migrate'], base_dir)
         django_operation(['migrate', '--run-syncdb'], base_dir)
-        django_operation([
-            'createsuperuser', '--noinput', '--email', '""'], base_dir)
         django_operation(['create_roles'], base_dir)
     except Exception:
         logger.exception('Cannot Migrate')
