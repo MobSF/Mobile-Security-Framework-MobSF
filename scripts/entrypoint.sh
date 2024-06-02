c600731b55e166750df4b160fac94bde102fcc13
Ajin Abraham <ajin.abraham@chime.com>
2024-06-02 16:14:36 -0700

    prevent entrypoint exit if username already exists

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 python3 manage.py makemigrations && \
 python3 manage.py makemigrations StaticAnalyzer && \
 python3 manage.py migrate
+set +e
 python3 manage.py createsuperuser --noinput --email ""
+set -e
 python3 manage.py create_roles
 
 exec gunicorn -b 0.0.0.0:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
