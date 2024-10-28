d817c0bf18b8360bf30fb9a3179194b065d1121e
Ajin Abraham <ajin.abraham@chime.com>
2024-10-28 03:35:56 -0700

    HOTFIX: Update wkhtmltopdf and dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,20 +7,13 @@ if [ "$TARGETPLATFORM" == "linux/arm64" ]
 then
     WKH_FILE=$WKH_FILE_ARM
     JDK_FILE=$JDK_FILE_ARM
-    LIBSSL_FILE=$LIBSSL_FILE_ARM
 fi
 
 echo "Target platform identified as $TARGETPLATFORM"
 JDK_URL="https://download.java.net/java/GA/jdk22.0.2/c9ecb94cd31b495da20a27d4581645e8/9/GPL/${JDK_FILE}"
-WKH_URL="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/${WKH_FILE}"
-LIBSSL11_URL="http://ftp.us.debian.org/debian/pool/main/o/openssl/${LIBSSL_FILE}"
+WKH_URL="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/${WKH_FILE}"
 
 # Download and install wkhtmltopdf
-# Install dependencies for wkhtmltopdf
-echo "Installing $LIBSSL_FILE ..."
-wget --quiet -O /tmp/${LIBSSL_FILE} "${LIBSSL11_URL}" && \
-    dpkg -i /tmp/${LIBSSL_FILE} && \
-    rm -f /tmp/${LIBSSL_FILE}
 
 echo "Installing $WKH_FILE ..."
 wget --quiet -O /tmp/${WKH_FILE} "${WKH_URL}" && \
